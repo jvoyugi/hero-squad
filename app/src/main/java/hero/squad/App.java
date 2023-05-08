ab09458329348d477258dd006b2d87c697547ddd
Javan Oyugi <jvoyugi@outlook.com>
2023-05-08 17:11:32 +0300

    remove username and password

--- app/src/main/java/hero/squad/App.java
+++ app/src/main/java/hero/squad/App.java
@@ -22,7 +22,7 @@ public class App {
     private static final String connectionString = System.getenv("DATABASE_URL");
 
     public static void main(String[] args) {
-        Sql2o sql2o = new Sql2o(connectionString, "postgres", "Logger33");
+        Sql2o sql2o = new Sql2o(connectionString);
         Sql2oHeroDAO heroDAO = new Sql2oHeroDAO(sql2o);
         Sql2oSquadDAO squadDAO = new Sql2oSquadDAO(sql2o);
         String port = System.getenv("PORT");
